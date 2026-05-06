b44e4c28c33155ff975cd2afb0297847503398b3
saranicwares <saran@icwares.com>
2026-05-06 16:16:01 +0530

    Update corefunctions.php

--- application/libraries/corefunctions.php
+++ application/libraries/corefunctions.php
@@ -267,10 +267,10 @@
               return false;
           }
       }
-      public function sendmail( $from, $to, $subject, $message ) {
+      public function sendmail( $from, $to, $subject, $message, $fromName = INDEXTITLE ) {
           $headers = "MIME-Version: 1.0" . "\r\n";
           $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-          $headers .= 'From: <' . $from . '>' . "\r\n";
+          $headers .= 'From: "' . $fromName . '" <' . $from . '>' . "\r\n";
           //$headers .= 'Cc: myboss@example.com' . "\r\n";
           mail( $to, $subject, $message, $headers );
       }
